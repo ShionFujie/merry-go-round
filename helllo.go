088c352f1c4c9f52b33f5aee6c130510d9871c13
shion.t.fujie <tonaluna32966@gmail.com>
2020-09-05 22:02:30 +0900

    comparison of strings

--- helllo.go
+++ helllo.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"example.com/user/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("Hello, SHION!!!"))
+	fmt.Println(cmp.Diff("Hello World", "Hello SHION"))
 }
