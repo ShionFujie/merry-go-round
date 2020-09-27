cd01c914f6ac66e1c1c2b54e1b35c904904941dc
shion.t.fujie <tonaluna32966@gmail.com>
2020-09-27 23:32:11 +0900

    shout out what's coming

---
+++ conc/reverb1.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"strings"
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+var port = flag.Int("port", 8000, "port number")
+
+func main() {
+	flag.Parse()
+	address := fmt.Sprintf("localhost:%d", *port)
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1*time.Second)
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	c.Close()
+}
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
